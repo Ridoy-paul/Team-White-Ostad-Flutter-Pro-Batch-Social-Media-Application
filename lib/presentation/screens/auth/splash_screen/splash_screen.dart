0e96549bfb752e5aa7ba0623344023199f3721ee
Sheikh Ajijul Hoque Shanto <57057476+mastershanto@users.noreply.github.com>
2024-04-26 01:22:38 +0600

    Splash Screen V1 added

--- lib/presentation/screens/auth/splash_screen/splash_screen.dart
+++ lib/presentation/screens/auth/splash_screen/splash_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:team_white_social_media_app/presentation/screens/auth/welcome_screen/welcome_screen.dart';
+
+import '../../../common_widgets/center_circular_progress_indicator.dart';
+import '../../home_screen/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -8,59 +13,36 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    moveToNextScreen();
+    }
+
+  // void moveToNextScreen() async {
+  //   await Future.delayed(const Duration(seconds: 1),);
+  //   await Get.find<AuthController>().initialize();
+  //   Get.offAll(const MainBottomNavScreen());
+  // }
+
+  void moveToNextScreen() async{
+    await Future.delayed(const Duration(seconds: 1),);
+    Get.offAll(()=>const WelcomeScreen());
+  }
+
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
+    return  Scaffold(
+      body:Center(
+        child:Column(
+          mainAxisSize: MainAxisSize.min,
           children: [
-            Text(
-              "Social Live",
-              style: Theme.of(context).textTheme.displaySmall,
-            ),
-            Center(child: Image.asset("assets/images/img.png")),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text("Add"),
-            ),
-            TextFormField(),
-             Text(
-              "titleLarge",
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.secondary,fontFamily: "Inter"),
-            ),
-            Text(
-              "titleMidium",
-              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).colorScheme.secondary,fontFamily: "Inter"),
-            ),
-            Text(
-              "Title Small",
-              style: Theme.of(context).textTheme.titleSmall,
-            ),
-            Text(
-              "Social Live",
-              style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.secondary,fontFamily: "satoshi",fontWeight: FontWeight.bold),
-            ),
-            Center(
-              child: Image.asset("assets/png_icons/Camera.png"),
-            ),
-            const Icon(
-              Icons.search,
-              size: 24,
-            ),
-            const Icon(
-              Icons.search,
-            ),
+            const CenterCircularProgressIndicator(),
+            Text("Social Live",style: Theme.of(context).textTheme.titleLarge?.copyWith(fontFamily: "Lobster",fontSize: 35),),
           ],
         ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home,),label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.search),label: "search"),
-          BottomNavigationBarItem(icon: Icon(Icons.add),label: "add"),
-          BottomNavigationBarItem(icon: Icon(Icons.person_2_outlined),label: "profile"),
-        ],
       ),
     );
   }
--- lib/presentation/screens/auth/welcome_screen/welcome_screen.dart
+++ lib/presentation/screens/auth/welcome_screen/welcome_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class WelcomeScreen extends StatelessWidget {
+class WelcomeScreen extends StatefulWidget {
   const WelcomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+  State<WelcomeScreen> createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen> {
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Text(
+              "Social Live",
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+            Center(child: Image.asset("assets/images/img.png")),
+            ElevatedButton(
+              onPressed: () {},
+              child: const Text("Add"),
+            ),
+            TextFormField(),
+            Text(
+              "titleLarge",
+              style: Theme.of(context).textTheme.titleLarge?.copyWith(color: Theme.of(context).colorScheme.secondary,fontFamily: "Inter"),
+            ),
+            Text(
+              "titleMidium",
+              style: Theme.of(context).textTheme.titleMedium?.copyWith(color: Theme.of(context).colorScheme.secondary,fontFamily: "Inter"),
+            ),
+            Text(
+              "Title Small",
+              style: Theme.of(context).textTheme.titleSmall,
+            ),
+            Text(
+              "Social Live",
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(color: Theme.of(context).colorScheme.secondary,fontFamily: "satoshi",fontWeight: FontWeight.bold),
+            ),
+            Center(
+              child: Image.asset("assets/png_icons/Camera.png"),
+            ),
+            const Icon(
+              Icons.search,
+              size: 24,
+            ),
+            const Icon(
+              Icons.search,
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home,),label: "Home"),
+          BottomNavigationBarItem(icon: Icon(Icons.search),label: "search"),
+          BottomNavigationBarItem(icon: Icon(Icons.add),label: "add"),
+          BottomNavigationBarItem(icon: Icon(Icons.person_2_outlined),label: "profile"),
+        ],
+      ),
+    );
   }
+
 }
