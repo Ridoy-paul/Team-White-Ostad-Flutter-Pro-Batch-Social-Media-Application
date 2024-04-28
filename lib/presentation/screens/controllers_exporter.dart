d379aee71b496070f2cd7e986d95babefc37ed9b
Sheikh Ajijul Hoque Shanto <57057476+mastershanto@users.noreply.github.com>
2024-04-29 01:01:10 +0600

    screen export to controller export updated

---
+++ lib/presentation/screens/controllers_exporter.dart
@@ -0,0 +1,20 @@
+
+export 'auth/create_profile_screen/state_holder/create_profile_controller.dart';
+export 'auth/email_or_phone_login_screen/state_holder/email_or_phone_login_controller.dart';
+export 'auth/otp_verification_screen/state_holder/otp_verification_controller.dart';
+export 'auth/send_otp_to_email_or_phone_screen/state_holder/send_otp_to_email_or_phone_controller.dart';
+export 'auth/splash_screen/state_holder/splash_controller.dart';
+export 'auth/welcome_screen/state_holder/welcome_screen_controller.dart';
+export 'content_upload_screen/state_holder/content_upload_controller.dart';
+export 'followers_screen/state_holder/followers_controller.dart';
+export 'following_screen/state_holder/following_controller.dart';
+export 'home_screen/state_holder/home_screen_controller.dart';
+export 'main_bottom_nav_screen/state_holder/main_bottom_nav_controller.dart';
+export 'messaging_screen/state_holder/messaging_controller.dart';
+export 'new_post_screen/state_holder/new_post_controller.dart';
+export 'notification_screen/state_holder/notification_controller.dart';
+export 'others_profile_screen/state_holder/others_profile_controller.dart';
+export 'profile_screen/state_holder/profile_controller.dart';
+export 'searching_screen/state_holder/searching_controller.dart';
+
+
