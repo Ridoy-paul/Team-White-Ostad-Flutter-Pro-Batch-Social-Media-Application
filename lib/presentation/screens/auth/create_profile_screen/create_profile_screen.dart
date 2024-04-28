7aee18b2ee2ee94e09a74465d490789c02ab03cc
Sheikh Ajijul Hoque Shanto <57057476+mastershanto@users.noreply.github.com>
2024-04-28 22:16:35 +0600

    Create Profile Screen Added

---
+++ lib/presentation/screens/auth/create_profile_screen/create_profile_screen.dart
@@ -0,0 +1,144 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../send_otp_to_email_or_phone_screen/send_otp_to_email_or_phone_screen.dart';
+
+class CreateProfileScreen extends StatefulWidget {
+  const CreateProfileScreen({super.key});
+
+  @override
+  State<CreateProfileScreen> createState() => _CreateProfileScreenState();
+}
+
+class _CreateProfileScreenState extends State<CreateProfileScreen> {
+  final TextEditingController _firstNameTEController = TextEditingController();
+  final TextEditingController _lastNameTEController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+
+  List<String> gender = ["Male", "Female", "Others"];
+  var selectedVal;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            Get.back();
+          },
+          icon: const Icon(Icons.arrow_back_ios),
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 48,
+                ),
+
+                Text(
+                  "Create Your Profile",
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      color: Theme.of(context).colorScheme.secondary,
+                      fontFamily: "Inter"),
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                Text(
+                  'Get started with us with your details',
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "First Name",
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                TextFormField(
+                  controller: _firstNameTEController,
+                  decoration: const InputDecoration(hintText: 'First name'),
+                  textInputAction: TextInputAction.next,
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Enter first name';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Text(
+                  "Last Name",
+                  style: Theme.of(context).textTheme.titleSmall,
+                ),
+                const SizedBox(
+                  height: 4,
+                ),
+                TextFormField(
+                  controller: _lastNameTEController,
+                  decoration: const InputDecoration(hintText: 'Last name'),
+                  textInputAction: TextInputAction.next,
+                  validator: (value) {
+                    if (value?.isEmpty ?? true) {
+                      return 'Enter last name';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  child: DropdownButton(
+                    hint: const Text("Chose Gender"),
+                    items: gender
+                        .map((item) => DropdownMenuItem(
+                            value: item,
+                            child: Text(item)))
+                        .toList(),
+                    value: selectedVal,
+                    onChanged: (value) {
+                      setState(() {
+                        selectedVal=value;
+                      });
+                    },
+                  ),
+
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Get.to(()=>SendOtpToEmailOrPhoneScreen);
+                      },
+                      child: const Text("Next"),
+                    )),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _firstNameTEController.dispose();
+    _lastNameTEController.dispose();
+    super.dispose();
+  }
+}
